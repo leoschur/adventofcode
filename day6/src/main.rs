3c28f2bf5f9bdfcbcecffa1dfda88e5e53efe7ff
Leo Schurrer <l.schurrer@oth-aw.de>
2023-12-06 17:19:57 +0100

    day6 gold

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
-    let times = [35, 93, 73, 66]; //  [7, 15, 30];
-    let distances = [212, 2060, 1201, 1044]; //  [9, 40, 200];
-    let mut nums = [0; 4];
+    let times: [u64; 1] = [35937366]; //  [7, 15, 30];
+    let distances: [u64; 1] = [212206012011044]; //  [9, 40, 200];
+    let mut nums: [u64; 1] = [0; 1];
 
     for (n, (&t, &d)) in std::iter::zip(nums.iter_mut(), times.iter().zip(distances.iter())) {
         println!("Race Time: {:3} Longest Distance: {:3}", t, d);
