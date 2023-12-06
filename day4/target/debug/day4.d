08abeb81907d2a92930796aaa8ac24ae3dce1647
Leo Schurrer <l.schurrer@oth-aw.de>
2023-12-06 11:04:39 +0100

    day4 silver

--- day4/target/debug/day4.d
+++
@@ -1,1 +0,0 @@
-A:\Projekt\adventofcode\day4\target\debug\day4.exe: A:\Projekt\adventofcode\day4\src\main.rs
--- day4/target/debug/deps/day4-3c2189112db46d2b.d
+++
@@ -1,5 +0,0 @@
-A:\Projekt\adventofcode\day4\target\debug\deps\day4-3c2189112db46d2b.rmeta: src\main.rs
-
-A:\Projekt\adventofcode\day4\target\debug\deps\day4-3c2189112db46d2b.d: src\main.rs
-
-src\main.rs:
